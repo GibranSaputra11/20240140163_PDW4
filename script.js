200f068ca78013b9cd9642a1cc7ca210e900d227
GibranSaputra11 <gibranbantul@gmail.com>
2026-03-16 15:04:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+Date()
+
+let tanggal = new Date()
+document.getElementById("tanggal").innerHTML = "📅 " + tanggal
+
+function tampil(){
+    alert("Yogyakarta adalah Daerah Istimewa yang terkenal sebagai pusat kebudayaan Jawa. Kota ini memiliki Keraton, batik, wayang, dan kuliner khas yang mendunia!")
+}
+
+function show(){
+    alert("Halo! Selamat datang di halaman Budaya Yogyakarta. Semoga kamu semakin mencintai budaya lokal kita!")
+}
+
+function infoBatik(){
+    alert("🎨 Batik Yogyakarta\n\nBatik Yogyakarta dikenal dengan warna dasar putih atau krem dengan corak gelap seperti hitam dan coklat. Motif terkenal antara lain:\n- Parang: simbol kekuatan\n- Kawung: simbol kesucian\n- Truntum: simbol cinta abadi")
+}
+
+function infoWayang(){
+    alert("🎭 Wayang Kulit\n\nWayang kulit adalah seni pertunjukan tradisional yang dimainkan oleh seorang Dalang. Pertunjukan bisa berlangsung semalam suntuk dan mengisahkan cerita dari epos Mahabharata atau Ramayana.")
+}
+
+function infoGudeg(){
+    alert("🍛 Gudeg Yogyakarta\n\nGudeg dibuat dari nangka muda yang dimasak berjam-jam dengan santan dan rempah-rempah. Biasanya disajikan dengan nasi, krecek, ayam kampung, dan telur pindang. Rasanya manis dan sangat khas!")
+}
